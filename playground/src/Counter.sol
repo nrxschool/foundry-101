393dd77a8c709f9569914acda3608d1d7c882265
Lucas Oliveira <olivmath@protonmail.com>
2024-10-15 23:38:48 -0300

    chore: forge init

--- playground/src/Counter.sol
+++ playground/src/Counter.sol
@@ -1,18 +1,14 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 count;
+    uint256 public number;
 
-    function incc() external {
-        count++;
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
     }
 
-    function set(uint256 number) external {
-        count = number;
-    }
-
-    function get() external view returns (uint256) {
-        return count;
+    function increment() public {
+        number++;
     }
 }
